c5a140a4b62049813ac36cfec32946fed2365622
RichardPitts1 <Richardepitts@gmail.com>
2020-07-13 15:39:24 -0500

    2

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -2,7 +2,28 @@ import React from 'react';
 
 const Profile = () => {
     return ( 
-        <h1>profile</h1>
+        <div>
+            <div style={{
+                display: "flex",
+                justifyContent: "space-around",
+                margin: "18px 0px",
+                borderBottom: "1px solid grey"
+            }}>
+                <div>
+                    <img style={{width: "160px", height: "160px", borderRadius: "80px"}} 
+                    src="https://images.unsplash.com/photo-1510915228340-29c85a43dcfe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80"  
+                    />
+                </div>
+                <div>
+                    <h4>Richard Pitts</h4>
+                    <div style={{display: "flex", justifyContent: "space-between", width: "108%"}}>
+                        <h6>40 posts</h6>
+                        <h6>40 followers</h6>
+                        <h6>40 following</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
      )
 }
 
