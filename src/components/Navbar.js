d9c630c1b36b37b31bad2e0327ad7b28b322ad09
RichardPitts1 <Richardepitts@gmail.com>
2020-07-13 14:08:02 -0500

    2

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const NavBar = () => {
+    return(
+        <nav>
+        <div className="nav-wrapper white">
+          <a href="/" className="brand-logo left">Codagram</a>
+          <ul id="nav-mobile" className="right">
+            <li><a href="/login">Login</a></li>
+            <li><a href="/signup">Signup</a></li>
+            <li><a href="/profile">Profile</a></li>
+          </ul>
+        </div>
+      </nav>
+    )
+}
+
+export default NavBar;
---
+++ src/components/screens/Signup.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Signup = () => {
+    return ( 
+        <h1>signup</h1>
+     )
+}
+
+export default Signup
