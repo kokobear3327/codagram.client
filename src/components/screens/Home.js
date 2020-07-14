424707c5b3cf0a4bb8b0a17d26eb24003f9f280e
RichardPitts1 <Richardepitts@gmail.com>
2020-07-14 17:35:00 -0500

    @

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,7 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
+import {UserContext} from '../../App' 
 
 const Home = () => {
     const [data, setData] = useState([])
+    const {state, dispatch} = useContext(UserContext)
     useEffect(()=>{
         fetch('/allpost', {
             headers:{
@@ -26,7 +28,16 @@ const Home = () => {
             })
         }).then(res=>res.json())
         .then(result=>{
-            console.log(result)
+            const newData = data.map(item=>{
+                if(item._id==result._id){
+                    return result
+                }else{
+                    return item
+                }
+            })
+            setData(newData)
+        }).catch(err=>{
+            console.log(err)
         })
     }
 
@@ -42,7 +53,16 @@ const Home = () => {
             })
         }).then(res=>res.json())
         .then(result=>{
-            console.log(result)
+            const newData = data.map(item=>{
+                if(item._id==result._id){
+                    return result
+                }else{
+                    return item
+                }
+            })
+            setData(newData)
+        }).catch(err=>{
+            console.log(err)
         })
     }
 
@@ -59,15 +79,19 @@ const Home = () => {
                     </div>
                     <div className="card-content">
                     <i className="material-icons" style={{color:"red"}}>favorite</i>
-                    <i className="material-icons"
+                    {item.likes.includes(state._id)
+                    ? <i className="material-icons"
+                            onClick={()=>{unlikePost(item._id)}}
+                        >thumb_down</i>
+                    : <i className="material-icons"
                     onClick={()=>{likePost(item._id)}}
                     >thumb_up</i>
-                    <i className="material-icons"
-                    onClick={()=>{unlikePost(item._id)}}
-                    >thumb_down</i>
+                    }
+  
+
                     <i className="material-icons">mood</i>
                     <i className="material-icons">mood_bad</i>
-                    <h6>{item.likes.length}</h6>
+                    <h6>{item.likes.length} likes</h6>
                     <h6>{item.title}</h6> 
                     <p>{item.body}</p>
                         <input type="text" placeholder="add a comment"/>
