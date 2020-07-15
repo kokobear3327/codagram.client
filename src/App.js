6348886b7df6ff78b23b8f9b7fea16f00cf43241
RichardPitts1 <Richardepitts@gmail.com>
2020-07-15 18:16:08 -0500

    @

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Signup from './components/screens/Signup'
 import CreatePost from './components/screens/CreatePost'
 import { reducer, initialState } from './reducers/userReducer'
 import UserProfile from './components/screens/UserProfile'
-
+import SubscribedUserPosts from './components/screens/SubscribesUserPosts'
 
 export const UserContext = createContext()
 
@@ -44,6 +44,9 @@ const Routing = ()=>{
     </Route>
     <Route path="/profile/:userid">
       <UserProfile />
+    </Route>
+    <Route path="/myfollowingpost">
+      <SubscribedUserPosts />
     </Route>
     </Switch>
   )
